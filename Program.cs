4db73029dbcd8e97fcf643f90351a319f5529ff9
Gianfranco Verrocchi <gianfranco@gmail.com>
2018-03-26 19:07:02 -0400

    Probando clase con otro programa exe

--- Program.cs
+++ Program.cs
@@ -7,11 +7,10 @@ class Program
 {
     static void Main()
     {
-        //LaunchCommandLineApp();
-        ExeRunner youtubeDl = new ExeRunner("youtube-dl.exe", "C:\\Users\\gian\\Music");
 
+        ExeRunner youtubeDl = new ExeRunner("youtube-dl.exe", "C:\\Users\\gian\\Music");
         List<string[]> parametros = new List<string[]>();
-        parametros.Add(new string[]{"--ffmpeg-location", "\"" + youtubeDl.WorkingDirectory+"ffmpeg.exe" + "\"" });
+        parametros.Add(new string[]{"--ffmpeg-location", "\"" + ExeRunner.StartupPath +"ffmpeg.exe" + "\"" });
         parametros.Add(new string[]{"--extract-audio"});
         parametros.Add(new string[]{"--audio-format","mp3"});
         parametros.Add(new string[]{"--no-playlist"}); 
@@ -19,6 +18,20 @@ class Program
 
         youtubeDl.Parameters = parametros;
         youtubeDl.Execute();
+
+        //Probando clase con otro ejecutable
+
+        // ExeRunner youtubeDl = new ExeRunner("RA2MD.exe", "C:\\Program Files (x86)\\Command And Conquer Red Alert 2\\", "C:\\Program Files (x86)\\Command And Conquer Red Alert 2\\");
+
+        // List<string[]> parametros = new List<string[]>();
+        // // parametros.Add(new string[]{"--ffmpeg-location", "\"" + ExeRunner.StartupPath +"ffmpeg.exe" + "\"" });
+        // // parametros.Add(new string[]{"--extract-audio"});
+        // // parametros.Add(new string[]{"--audio-format","mp3"});
+        // // parametros.Add(new string[]{"--no-playlist"}); 
+        // // parametros.Add(new string[]{"https://www.youtube.com/watch?v=bpOSxM0rNPM"});
+
+        // youtubeDl.Parameters = parametros;
+        // youtubeDl.Execute();
     }
 
 
